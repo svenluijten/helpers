e2611a077f3633cdcf5d5231f4fccb53778210d7
Sven Luijten <svenluijten1995@gmail.com>
2017-03-12 17:40:13 +0100

    Add base test suite

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -6,31 +6,5 @@ use GrahamCampbell\TestBench\AbstractPackageTestCase;
 
 abstract class TestCase extends AbstractPackageTestCase
 {
-    /**
-     * Get the service provider class.
-     *
-     * @param \Illuminate\Contracts\Foundation\Application $app
-     *
-     * @return string
-     */
-    protected function getServiceProviderClass($app)
-    {
-        //
-    }
-
-    /**
-     * Set up the testing suite.
-     */
-    public function setUp()
-    {
-        //
-    }
-
-    /**
-     * Tear down the testing suite.
-     */
-    public function tearDown()
-    {
-        //
-    }
+    //
 }
